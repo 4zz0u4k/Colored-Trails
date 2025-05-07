292cd8946b8f1afaa427ba81d64d912f432c4891
unknown <la_akeb@esi.dz>
2025-05-07 11:31:51 +0100

    added cells manipulation logic

---
+++ src/ColorTrailAgent.java
@@ -0,0 +1,21 @@
+import jade.core.Agent;
+
+public class ColorTrailAgent extends Agent {
+    private Environment env;
+    private int x, y;
+    public Cell position;
+    @Override
+    protected void setup() {
+        Object[] args = getArguments();
+        if (args != null && args.length >= 1) {
+            env = (Environment) args[0];
+            position = env.getRandomInitialCell();
+
+            System.out.println(getLocalName() + " started at position (" + position.x + ", " + position.y + ")");
+            System.out.println("Path color here: " + position.color);
+        } else {
+            System.out.println("Missing arguments for " + getLocalName());
+            doDelete(); // terminate the agent
+        }
+    }
+}
--- src/Environment.java
+++ src/Environment.java
@@ -1,8 +1,11 @@
+import java.util.ArrayList;
+
 public class Environment {
     public static final int WIDTH = 7;
     public static final int HEIGHT = 5;
     private final Cell[][] grid = new Cell[WIDTH][HEIGHT];
-
+    private ArrayList<Cell> freeCells = new ArrayList<>(WIDTH * HEIGHT);
+    private ArrayList<Cell> occupiedCells = new ArrayList<>();
     public Environment() {
         initializeGrid();
     }
@@ -15,6 +18,31 @@ public class Environment {
                 grid[x][y] = new Cell(x, y, color);
             }
         }
+        for (int x = 0; x < WIDTH * HEIGHT; x++) {
+            freeCells.add(x,grid[x / HEIGHT][x % WIDTH]);
+        }
+    }
+
+    public Cell getRandomInitialCell(){
+        int randomNum = (int)(Math.random() * freeCells.size());
+        Cell cell = freeCells.get(randomNum);
+        occupiedCells.add(cell);
+        freeCells.remove(randomNum);
+        return cell;
+    }
+
+    public Cell occupyCell(int x, int y) {
+        Cell cell = getCell(x, y);
+        occupiedCells.add(cell);
+        freeCells.remove(x * WIDTH + y);
+        return cell;
+    }
+
+    public Cell freeCell(int x, int y) {
+        Cell cell = getCell(x, y);
+        freeCells.add(cell);
+        occupiedCells.remove(cell);
+        return cell;
     }
 
     public Cell getCell(int x, int y) {
