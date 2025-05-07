703d0d488e000e3b4e85b12267ca7ba0378dd8e9
unknown <la_akeb@esi.dz>
2025-05-07 11:39:16 +0100

    added goal cell logic

--- src/Environment.java
+++ src/Environment.java
@@ -3,24 +3,30 @@ import java.util.ArrayList;
 public class Environment {
     public static final int WIDTH = 7;
     public static final int HEIGHT = 5;
+    private Cell goal;
     private final Cell[][] grid = new Cell[WIDTH][HEIGHT];
     private ArrayList<Cell> freeCells = new ArrayList<>(WIDTH * HEIGHT);
     private ArrayList<Cell> occupiedCells = new ArrayList<>();
-    public Environment() {
-        initializeGrid();
+    public Environment(int goalX, int goalY) {
+        initializeGrid(goalX, goalY);
     }
 
-    private void initializeGrid() {
+    private void initializeGrid(int goal_x, int goal_y) {
         PathColor[] colors = PathColor.values();
         for (int x = 0; x < WIDTH; x++) {
             for (int y = 0; y < HEIGHT; y++) {
                 PathColor color = colors[(x + y) % colors.length];
                 grid[x][y] = new Cell(x, y, color);
+                if (x == goal_x && y == goal_y) {
+                   goal = grid[x][y];
+                }
             }
         }
+        occupiedCells.add(goal);
         for (int x = 0; x < WIDTH * HEIGHT; x++) {
             freeCells.add(x,grid[x / HEIGHT][x % WIDTH]);
         }
+        freeCells.remove(goal);
     }
 
     public Cell getRandomInitialCell(){
