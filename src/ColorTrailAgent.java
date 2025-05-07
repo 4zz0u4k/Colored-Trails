4a8685e4938774a2ca52b669c98a0c2db0d2d978
unknown <la_akeb@esi.dz>
2025-05-07 11:55:19 +0100

    just to save to logic

--- src/ColorTrailAgent.java
+++ src/ColorTrailAgent.java
@@ -4,13 +4,14 @@ public class ColorTrailAgent extends Agent {
     private Environment env;
     private int x, y;
     public Cell position;
+    private AgentResources resources;
     @Override
     protected void setup() {
         Object[] args = getArguments();
-        if (args != null && args.length >= 1) {
+        if (args != null && args.length >= 2) {
             env = (Environment) args[0];
             position = env.getRandomInitialCell();
-
+            resources = (AgentResources) args[1];
             System.out.println(getLocalName() + " started at position (" + position.x + ", " + position.y + ")");
             System.out.println("Path color here: " + position.color);
         } else {
